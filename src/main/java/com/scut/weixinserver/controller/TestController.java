3f85522f50da00d4168e722be0705c94de0089fe
huangtiansheng <873966702@qq.com>
2018-04-14 16:42:38 +0800

    文件上传

--- src/main/java/com/scut/weixinserver/controller/TestController.java
+++ src/main/java/com/scut/weixinserver/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.scut.weixinserver.controller;
 
 import java.io.FileNotFoundException;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.scut.weixinserver.model.APIBodyData;
 import com.scut.weixinserver.model.PageData;
@@ -21,6 +24,16 @@ public class TestController extends BaseController {
     @ResponseBody
     public ResultBean test(APIBodyData data) throws FileNotFoundException, Exception{  
       logger.info("test测试："+data.toString());
+      ResultBean result=new ResultBean();
+      result.setData("cao ni ma");
+      return result;
+       
+    }  
+    @RequestMapping("/MultipartTest") 
+    @ResponseBody
+    public ResultBean multipartTest(@RequestParam(value = "fileList", required = false) List<MultipartFile> files,String test ) throws FileNotFoundException, Exception{  
+      logger.info("test测试："+test);
+      logger.info("test测试："+test);
       ResultBean result=new ResultBean();
       result.setData("cao ni ma");
       return result;
--- src/main/java/com/scut/weixinserver/filter/DecryptFilter.java
+++ src/main/java/com/scut/weixinserver/filter/DecryptFilter.java
@@ -40,9 +40,10 @@ import com.scut.weixinserver.utils.AES;
 @Order(1)
 @WebFilter(filterName = "DecryptFilter", urlPatterns = "/*")
 public class DecryptFilter implements Filter {
-	   private static final Set<String> ALLOWED_PATHS = Collections.unmodifiableSet(new HashSet<>(  
-	            Arrays.asList("/key/keyExchange")));  
+	private static final Set<String> ALLOWED_PATHS = Collections
+			.unmodifiableSet(new HashSet<>(Arrays.asList("/key/keyExchange")));
 	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -52,44 +53,49 @@ public class DecryptFilter implements Filter {
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
 			throws IOException, ServletException {
 		logger.info("——————————————————解密filter开始————————————————");
-		
+
 		HttpServletRequest request = (HttpServletRequest) servletRequest;
 		HttpServletResponse response = (HttpServletResponse) servletResponse;
-		if(request.getCookies()!=null)
-		logger.info(request.getCookies().toString());
-		//排除不需要解密的url
-		 String path = request.getRequestURI().substring(request.getContextPath().length()).replaceAll("[/]+$", "");  
-	        boolean allowedPath = ALLOWED_PATHS.contains(path);  
-	        if (allowedPath) {  
-	        	logger.info("——————————————————解密filter结束————————————————");
-	        	filterChain.doFilter(request, response);  
-	        	return;
-	        }  
-	    //解密操作
-		BodyReaderRequestWrapper requestWrapper = new BodyReaderRequestWrapper(request);
-		ResponseBodyWrapper wrapResponse = new ResponseBodyWrapper(
-                (HttpServletResponse) response);
-		if (requestWrapper.tryDecrypt(requestWrapper)){
-			
-			filterChain.doFilter(requestWrapper, wrapResponse);
-		//加密操作
-		byte[] data = wrapResponse.getResponseData();
-		
-        logger.info("原始返回数据： " + new String(data, "utf-8"));
-        String AESkey=(String) request.getSession().getAttribute("AESkey");
-        // 加密返回报文
-    	logger.info("——————————————————加密filter开始————————————————");
-        String responseBodyMw = AES.encrypt(new String(data, "utf-8"), AESkey);
-        EncryptBean encryptBean=new EncryptBean();
-        encryptBean.setSuccess(true);
-        encryptBean.setEncyptData(responseBodyMw);
-       String finalMessage= new Gson().toJson(encryptBean);
-        logger.info("加密返回数据： " +   finalMessage);
-        
-        writeResponse(response, finalMessage);
-        logger.info("——————————————————加密filter结束————————————————");
+		if (request.getCookies() != null)
+			logger.info(request.getCookies().toString());
+		// 排除不需要解密的url
+		String path = request.getRequestURI().substring(request.getContextPath().length()).replaceAll("[/]+$", "");
+		boolean allowedPath = ALLOWED_PATHS.contains(path);
+		//如果是不需要加密解密的地址，正常进入controller
+		if (allowedPath) {
+			logger.info("——————————————————解密filter结束————————————————");
+			filterChain.doFilter(request, response);
+			return;
 		}
-		else {
+		//如果是multipart/form-data类型，不进行加密解密
+		if(request.getContentType().contains("multipart/form-data")){
+			logger.info("multipart/form-data类型，不需要加解密！！");
+			logger.info("——————————————————解密filter结束————————————————");
+			filterChain.doFilter(request, response);
+			return;
+		}
+		// application/json类型进行解密操作
+		BodyReaderRequestWrapper requestWrapper = new BodyReaderRequestWrapper(request);
+		ResponseBodyWrapper wrapResponse = new ResponseBodyWrapper((HttpServletResponse) response);
+		//如果存在SESSION中存在AES key，流程正常进行
+		if (requestWrapper.tryDecrypt(requestWrapper)) {
+			filterChain.doFilter(requestWrapper, wrapResponse);
+			// 加密操作
+			byte[] data = wrapResponse.getResponseData();
+			logger.info("原始返回数据： " + new String(data, "utf-8"));
+			String AESkey = (String) request.getSession().getAttribute("AESkey");
+			// 加密返回报文
+			logger.info("——————————————————加密filter开始————————————————");
+			String responseBodyMw = AES.encrypt(new String(data, "utf-8"), AESkey);
+			EncryptBean encryptBean = new EncryptBean();
+			encryptBean.setSuccess(true);
+			encryptBean.setEncyptData(responseBodyMw);
+			String finalMessage = new Gson().toJson(encryptBean);
+			logger.info("加密返回数据： " + finalMessage);
+
+			writeResponse(response, finalMessage);
+			logger.info("——————————————————加密filter结束————————————————");
+		} else {
 			logger.warn("注意：获取AES KEY失败！！！");
 			PrintWriter writer = null;
 			OutputStreamWriter osw = null;
@@ -104,15 +110,16 @@ public class DecryptFilter implements Filter {
 			osw.close();
 			logger.info("——————————————————解密filter结束————————————————");
 		}
-				
+
 	}
-	private void writeResponse(ServletResponse response, String responseString)
-	        throws IOException {
-	    PrintWriter out = response.getWriter();
-	    out.print(responseString);
-	    out.flush();
-	    out.close();
+
+	private void writeResponse(ServletResponse response, String responseString) throws IOException {
+		PrintWriter out = response.getWriter();
+		out.print(responseString);
+		out.flush();
+		out.close();
 	}
+
 	@Override
 	public void destroy() {
 
